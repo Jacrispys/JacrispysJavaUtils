8602aec8e99842b63cd80eee6f92bcfab2fe7c81
Jacrispys <jvanzyverden@icloud.com>
2022-11-28 15:40:17 -0800

    Char mode util

--- src/main/java/UtilTesting.java
+++ src/main/java/UtilTesting.java
@@ -3,6 +3,6 @@ import utils.*;
 public class UtilTesting {
 
     public static void main(String[] args) {
-
+        System.out.println(CollectionUtils.findMode("Testingsg"));
     }
 }
--- src/main/java/utils/CollectionUtils.java
+++ src/main/java/utils/CollectionUtils.java
@@ -20,13 +20,13 @@ public class CollectionUtils {
         return set;
     }
 
-    public static <T extends CharSequence> Set<T> findMode(T entry) {
-        HashMap<T, Integer> entryMap = new HashMap<>();
-        entry.chars().forEach((CharSequence) vara -> entryMap.put(vara, entryMap.getOrDefault(vara, 0) + 1));
-        Set<T> set = new HashSet<>();
+    public static <T extends CharSequence> Set<Character> findMode(T entry) {
+        Map<Character, Integer> entryMap = new HashMap<>();
+        entry.chars().forEach(vara -> entryMap.put((char) vara, entryMap.getOrDefault((char) vara, 0) + 1));
+        Set<Character> set = new HashSet<>();
         int highestValue = Collections.max(entryMap.entrySet(), Map.Entry.comparingByValue()).getValue();
         do {
-            Map.Entry<T, Integer> mapEntry = Collections.max(entryMap.entrySet(), Map.Entry.comparingByValue());
+            Map.Entry<Character, Integer> mapEntry = Collections.max(entryMap.entrySet(), Map.Entry.comparingByValue());
             if (mapEntry.getValue() == highestValue) {
                 set.add(mapEntry.getKey());
             }
