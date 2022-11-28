f7ec6d2757c04c7ef7bca5a69341063ba3778253
Jacrispys <jvanzyverden@icloud.com>
2022-11-28 15:48:40 -0800

    Char mode util

--- src/main/java/utils/CollectionUtils.java
+++ src/main/java/utils/CollectionUtils.java
@@ -6,8 +6,8 @@ import java.util.*;
 public class CollectionUtils {
 
     public static <S, T extends Collection<S>> Set<S> findMode(T entry) {
-        HashMap<S, Integer> entryMap = new HashMap<>();
-        entry.forEach(vara -> entryMap.put(vara, entryMap.getOrDefault(vara, 0) + 1));
+        Map<S, Integer> entryMap = new HashMap<>();
+        entry.forEach(vara -> entryMap.merge(vara, 1, Integer::sum));
         Set<S> set = new HashSet<>();
         int highestValue = Collections.max(entryMap.entrySet(), Map.Entry.comparingByValue()).getValue();
         do {
@@ -22,7 +22,7 @@ public class CollectionUtils {
 
     public static <T extends CharSequence> Set<Character> findMode(T entry) {
         Map<Character, Integer> entryMap = new HashMap<>();
-        entry.chars().forEach(vara -> entryMap.put((char) vara, entryMap.getOrDefault((char) vara, 0) + 1));
+        entry.chars().forEach(vara -> entryMap.merge((char) vara, 1, Integer::sum));
         Set<Character> set = new HashSet<>();
         int highestValue = Collections.max(entryMap.entrySet(), Map.Entry.comparingByValue()).getValue();
         do {
